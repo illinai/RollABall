4204377ef53ec095358eb715c009b785bf2b5f2c
illinai <144272615+illinai@users.noreply.github.com>
2025-01-28 22:22:35 -0800

    colour

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -2,31 +2,36 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Rigidbody sphereRigidbody;
+
+    
     void Start()
     { }
 
-    // Update is called once per frame
     void Update()
     {
+        Vector2 inputVector = Vector2.zero;
+
         if (Input.GetKey(KeyCode.W))
         {
-            Debug.Log("User's Input: W");
+            inputVector += Vector2.up;
         }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            Debug.Log("User's Input: W");
-        }
-
         if (Input.GetKey(KeyCode.S))
         {
-            Debug.Log("User's Input: W");
+            inputVector += Vector2.down;
         }
-
         if (Input.GetKey(KeyCode.D))
         {
-            Debug.Log("User's Input: W");
+            inputVector += Vector2.right;
         }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector += Vector2.left;
+        }
+
+        Vector3 inputXZPlane = new Vector3(inputVector.x, 0, inputVector.y);
+        Debug.Log("Resultant Vector: " + inputVector);
+        Debug.Log("Resultant 3D Vector " + inputXZPlane);
+        sphereRigidbody.AddForce(inputXZPlane);
     }
 }
